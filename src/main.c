04cb44f62096d2786e3be1e55892ff771ca53893
Ori Mizrahi <ori.mizrahi1000@gmail.com>
2026-03-22 23:21:43 +0200

    Simple fork debugee

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(void) {
-    printf("Yet Another Debugger!\n");
+int main(int argc, char *argv[], char *envp[]) {
+    if(argc == 1) {
+        fprintf(stderr, "USAGE: yadb [TARGET] [ARGS]\n");
+        return 1;
+    }
+
+
+    pid_t debugee = fork();
+    if(debugee == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+
+        if(execve(argv[1], argv + 1, envp) == -1)
+            perror("execve\n");
+    } else {
+        int status;
+
+        wait(&status);
+        printf("Program status: %d\n", status);
+    }
 
     return 0;
 }
-
